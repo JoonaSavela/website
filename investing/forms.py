7141921f00bd45ba450d00222241585aa1a91475
Joona Savela <joona.savela@aalto.fi>
2019-04-18 23:04:20 +0300

    make first project better

--- investing/forms.py
+++ investing/forms.py
@@ -1,5 +1,46 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 class InvestingForm(forms.Form):
-    initialAmount = forms.IntegerField(required=True, min_value=0, label='Initial Amount')
-    years = forms.IntegerField(required=True, min_value=0, label='Years')
+    annualInterestRate = forms.FloatField(required=True,
+                            min_value=1.0,
+                            label='Annual Interest Rate',
+                            initial=1.07,
+                            widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    monthlyAmount = forms.FloatField(required=True,
+                            min_value=0,
+                            label='Monthly Amount',
+                            initial=1500,
+                            widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    initialCapital = forms.FloatField(required=True,
+                            min_value=0,
+                            label='Initial Capital',
+                            initial=10_000,
+                            widget=forms.NumberInput(attrs={'class': 'form-control'}))
+
+    carPrice = forms.FloatField(required=True,
+                            min_value=0,
+                            label='Car Price',
+                            initial=71_228,
+                            widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    carLoanAnnualInterestRate = forms.FloatField(required=True,
+                            min_value=1.0,
+                            label='Car Loan Annual Interest Rate',
+                            initial=1.019,
+                            widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    monthlyCarLoanPayment = forms.FloatField(required=True,
+                            min_value=0,
+                            label='Monthly Car Loan Payment',
+                            initial=614,
+                            widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    payWithInitialCapital = forms.BooleanField(label='Pay With Initial Capital',
+                            required=False,
+                            widget=forms.CheckboxInput(attrs={'class': 'form-control'}))
+
+    def clean(self):
+        cd = self.cleaned_data
+
+        if cd.get('monthlyCarLoanPayment') > cd.get('monthlyAmount'):
+            raise ValidationError("Monthly car loan payment cannot be grater than monthly amount.")
+
+        return cd
--- investing/views.py
+++ investing/views.py
@@ -1,24 +1,64 @@
 from django.shortcuts import render
+from math import log
 
 from .forms import InvestingForm
+
+def geomSum(a, q, k):
+    if q == 1:
+        return a * k
+    return a * (1 - q ** k) / (1 - q)
+
+def capitalGain(c, q, k):
+    return c * q ** k
+
+def getTime(monthlyCarLoanPayment, carPrice, carLoanAnnualInterestRate):
+    base = carLoanAnnualInterestRate ** (1 / 12)
+    frac = monthlyCarLoanPayment / (carPrice * (1 - base) + monthlyCarLoanPayment)
+    return log(frac, base)
 
 # Create your views here.
 def index(request):
     amount = 0
     show = False
+    years = 0
+    months = 0
+    carLoanInterestPaid = 0
     if request.method == 'POST':
         form = InvestingForm(request.POST)
         if form.is_valid():
-            initialAmount = form.cleaned_data['initialAmount']
-            years = form.cleaned_data['years']
-            amount = initialAmount * years * 12
-            show = True
+            annualInterestRate = form.cleaned_data['annualInterestRate']
+            monthlyAmount = form.cleaned_data['monthlyAmount']
+            capital = form.cleaned_data['initialCapital']
+            carPrice = form.cleaned_data['carPrice']
+            carLoanAnnualInterestRate = form.cleaned_data['carLoanAnnualInterestRate']
+            monthlyCarLoanPayment = form.cleaned_data['monthlyCarLoanPayment']
+            payWithInitialCapital = form.cleaned_data['payWithInitialCapital']
 
+            if payWithInitialCapital:
+                newCarPrice = max(carPrice - capital, 0)
+                capital -= carPrice - newCarPrice
+                carPrice = newCarPrice
+
+            months = getTime(monthlyCarLoanPayment, carPrice, carLoanAnnualInterestRate)
+
+            carLoanInterestPaid = round(months * monthlyCarLoanPayment - carPrice, 2)
+
+            years = round(months // 12)
+            months = round(months - years * 12)
+
+            amount = geomSum(monthlyAmount - monthlyCarLoanPayment, annualInterestRate ** (1 / 12), years * 12 + months)
+            amount += capitalGain(capital, annualInterestRate ** (1 / 12), years * 12 + months)
+
+            show = True
     else:
         form = InvestingForm()
+
     context = {
         'form': form,
-        'amount': amount,
+        'years': years,
+        'months': months,
+        'amount': round(amount, 2),
+        'carLoanInterestPaid': carLoanInterestPaid,
         'show': show,
     }
     return render(request, 'investing/index.html', context)
